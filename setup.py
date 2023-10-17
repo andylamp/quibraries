6b3347137499ca5790f70bac00171eece76252e1
andylamp <andreas.grammenos@gmail.com>
2023-10-17 03:59:26 +0300

    fix content type

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 from setuptools import find_packages, setup
 
-VERSION: str = "1.0.5"
+VERSION: str = "1.0.6"
 
 # get the current path
 CURRENT_PATH = os.path.dirname(os.path.abspath(__file__))
@@ -29,7 +29,7 @@ if __name__ == "__main__":
         author_email="axorl@quine.sh",
         description="A thread-safe Python wrapper for the libraries.io API",
         long_description=long_desc,
-        long_description_content_type="text/rst",
+        long_description_content_type="text/x-rst",
         url="https://github.com/andylamp/quibraries/",
         packages=find_packages(),
         install_requires=requirements,
